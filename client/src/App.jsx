3b724a7292da1aaf869a2272529cce286860a943
Momin Ali <mominali1296@gmail.com>
2024-08-25 23:16:36 +0200

    fixes typo in App.jsx imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Layout from "./pages/Layout";
 import Login from "./pages/users/Login";
 import Register from "./pages/users/Register";
 import Dashboard from "./pages/users/Dashboard";
-import Home from "./pages/posts/home";
+import Home from "./pages/posts/Home";
 import Create from "./pages/posts/Create";
 import Update from "./pages/posts/Update";
 import AuthRoutes from "./routes/Authroutes";
