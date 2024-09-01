cc67c5da077a2af164401ac7f7c52109616d6d4a
Momin Ali <mominali1296@gmail.com>
2024-09-01 13:18:17 +0200

    fixes navbar issue

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -19,7 +19,7 @@ const Layout = () => {
   return (
     <>
       <header className="bg-indigo-500 text-white">
-        <nav className="flex items-center justify-between p-4 max-w-screen-lg">
+        <nav className="flex items-center justify-between p-4 max-w-screen-lg mx-auto">
           <Link
             title="homepage"
             to="/"
